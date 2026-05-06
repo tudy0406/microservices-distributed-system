066ea38d683a35f6877c8dd22cc062bd02c02791
Tudy0406 <radtudor7@gmail.com>
2026-05-06 15:14:42 +0300

    chore: add suggestions microservice project folder

---
+++ suggestions_service/src/test/java/pt/project/suggestions_service/SuggestionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.project.suggestions_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

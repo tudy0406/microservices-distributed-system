74ab42ec9f464a6b292fb6d119d187a89be3bd5f
Tudy0406 <radtudor7@gmail.com>
2026-05-06 15:17:09 +0300

    chore: add movie service project folder

---
+++ movie_service/src/main/java/pt/project/microservice/MoveServiceApplication.java
@@ -0,0 +1,13 @@
+package pt.project.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoveServiceApplication.class, args);
+    }
+
+}

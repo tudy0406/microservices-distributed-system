9751ce957673252a8c199ec292bb0af248995d25
Tudy0406 <radtudor7@gmail.com>
2026-05-06 15:46:25 +0300

    refactor: rename application MoveService -> MovieService

--- movie_service/src/main/java/pt/project/microservice/MovieServiceApplication.java
+++ movie_service/src/main/java/pt/project/microservice/MovieServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoveServiceApplication {
+public class MovieServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MoveServiceApplication.class, args);
+        SpringApplication.run(MovieServiceApplication.class, args);
     }
 
 }

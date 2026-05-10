f54732a8cf3300e691772c7fe3e6e726e28bf035
Tudy0406 <radtudor7@gmail.com>
2026-05-10 17:45:34 +0300

    feat(gateway poc): add gatewayservice vanilla implementation for poc
    - implement a search method which gets the query and send it to the movie service through api

---
+++ api_gateway/src/main/java/pt/project/api_gateway/GatewayService.java
@@ -0,0 +1,25 @@
+package pt.project.api_gateway;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class GatewayService {
+
+    private final RestTemplate restTemplate;
+
+    public GatewayService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String search(String query) {
+
+        String url = UriComponentsBuilder
+                .fromUriString("http://localhost:8081/movie/search")
+                .queryParam("query", query)
+                .toUriString();
+
+        return restTemplate.getForObject(url, String.class);
+    }
+}

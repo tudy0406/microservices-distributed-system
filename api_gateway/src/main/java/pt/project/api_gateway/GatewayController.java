a499b9cdaf50b003a3d9643440b4d85488e500cd
Tudy0406 <radtudor7@gmail.com>
2026-05-10 17:42:36 +0300

    feat(gateway poc): add controller with vanilla implementation for poc
    - gets a GET request and returns an ok response

--- api_gateway/src/main/java/pt/project/api_gateway/GatewayController.java
+++ api_gateway/src/main/java/pt/project/api_gateway/GatewayController.java
@@ -3,18 +3,20 @@ package pt.project.api_gateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
+@RequestMapping("/gateway")
 public class GatewayController {
 
-    @Autowired
     private final GatewayService service;
 
     public GatewayController(GatewayService service) {
         this.service = service;
     }
 
-    @GetMapping("/movie")
-    public String query(@RequestBody String query) {
-        return service.search(query);
+    @GetMapping("/movies")
+    public String query(@RequestParam String query) {
+        //return service.search(query);
+        return "ok";
     }
 }

ba88a5053d39d0ba89e22ca1b5cdedaef700e1f1
Tudy0406 <radtudor7@gmail.com>
2026-05-12 20:32:03 +0300

    feat(resilience): add fix timeout to restTamplate response waiting

--- movie_service/src/main/java/pt/project/microservice/config/AppConfig.java
+++ movie_service/src/main/java/pt/project/microservice/config/AppConfig.java
@@ -2,6 +2,7 @@ package pt.project.microservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import pt.project.microservice.db.DatabaseManager;
 
@@ -10,7 +11,13 @@ public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        factory.setConnectTimeout(1500);
+        factory.setReadTimeout(1500);
+
+        return new RestTemplate(factory);
     }
 
     @Bean

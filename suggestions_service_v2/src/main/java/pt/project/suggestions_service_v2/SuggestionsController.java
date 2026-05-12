90c1982b92638cb3c128a2a2435dc62ab7a4fa2c
Tudy0406 <radtudor7@gmail.com>
2026-05-12 20:23:41 +0300

    feat(error simulation): add random delay in suggestionService Controller to simulate errors

--- suggestions_service_v2/src/main/java/pt/project/suggestions_service_v2/SuggestionsController.java
+++ suggestions_service_v2/src/main/java/pt/project/suggestions_service_v2/SuggestionsController.java
@@ -1,10 +1,13 @@
 package pt.project.suggestions_service_v2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Random;
 
 @CrossOrigin(origins = "http://localhost:8082")
 @RequestMapping("/suggestions")
@@ -18,6 +21,29 @@ public class SuggestionsController {
 
     @GetMapping("/search")
     public ResponseEntity<List<Integer>> getSuggestions(@RequestParam("movieId") int id){
+        String chaos = System.getenv("CHAOS_MODE");
+        try{
+            if ("true".equalsIgnoreCase(chaos)) {
+
+                Random random = new Random();
+
+                // Random latency
+                int delay = 3000 + random.nextInt(7000);
+
+                Thread.sleep(delay);
+
+                // Random failure
+                if (random.nextBoolean()) {
+                    throw new ResponseStatusException(
+                            HttpStatus.SERVICE_UNAVAILABLE,
+                            "Chaos mode failure"
+                    );
+                }
+            }
+        }catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+
         List<Integer> response = suggestionsService.searchSuggestions(id);
         return ResponseEntity.ok(response);
     }
